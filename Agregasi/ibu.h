d5cb549c43fc43c40657d57e08b0ea320ad68bd0
Yurcell <chestayurcel@gmail.com>
2024-05-20 15:01:14 +0700

    membuat class ibu

--- Agregasi/ibu.h
+++ Agregasi/ibu.h
@@ -1,2 +1,13 @@
 #ifndef IBU_H
 #define IBU_H
+
+class ibu {
+public:
+    string nama;
+    vector<anak*> daftar_anak;
+
+    ibu(string pNama) :nama(pNama) {
+        cout << "Ibu \"" << nama << "\" ada\n";
+    }
+    
+}
